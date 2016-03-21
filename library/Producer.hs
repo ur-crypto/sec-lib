ff0d64d5f9e3b0c0259008521657fbf4379dd99b
Jacob Bisnett <jacob.bisnett@gmail.com>
2016-03-21 17:58:16 -0400

    before big change

--- library/Producer.hs
+++ library/Producer.hs
@@ -1,18 +1,16 @@
 module Producer where
-import Data.Bits
-import Data.Word
 import qualified Data.ByteString as B
+import Data.ByteString.Internal (unpackBytes)
 import qualified Data.ByteString.Char8 as B8
+import qualified Codec.Binary.BubbleBabble as X
+import qualified Codec.Encryption.AES as C
+import Codec.Utils
+import Data.LargeWord
 import Utils
 
-type Key = B.ByteString
+type Key = Word128
 data KeyPair = KeyPair{key1::Key, key2::Key}
-data TruthTable = TruthTable{
-      row1 :: (Key, Key, Key)
-    , row2 :: (Key, Key, Key) 
-    , row3 :: (Key, Key, Key)
-    , row4 :: (Key, Key, Key)
-    }
+data TruthTable = TruthTable (Key, Key, Key) (Key, Key, Key) (Key, Key, Key) (Key, Key, Key)
 
 printTT :: TruthTable -> IO()
 printTT (TruthTable row1 row2 row3 row4 )= do
@@ -24,24 +22,34 @@ printTT (TruthTable row1 row2 row3 row4 )= do
     where
     printRow :: (Key, Key, Key) -> IO()
     printRow (k1, k2, k3) = do
-        print $ show (B8.unpack k1) ++ " " ++ show (B8.unpack k2) ++ " " ++ show (B8.unpack k3)
+        let s_list = map (X.encode . unpackBytes) [k1, k2, k3]
+        print $ unwords s_list
         return ()
 
+unwrapKeys :: IO(Key, Key) -> IO(Key, Key) -> IO(Key, Key, Key, Key)
+unwrapKeys kp1 kp2 = do
+    (a0, a1) <- kp1
+    (b0, b1) <- kp2
+    return (a0, a1, b0, b1)
+
+encryptKey :: (Word128, Word128, Word128) -> Word128
+encryptKey (k0, k1, m) = C.encrypt k0 . C.encrypt k1 m
+
+printSendTT :: TruthTable -> IO()
+printSendTT (TruthTable r1 r2 r3 r4) = 
+    let list = map encryptKey [r1, r2, r3, r4] in
+    print $ unlines list
 
 (.&.) :: IO (Key, Key) -> IO(Key, Key) -> IO (Key, Key)
 (.&.) kp1 kp2 = do
-    (a0, a1) <- kp1
-    (b0, b1) <- kp2
-    o0 <- getKey
-    o1 <- getKey
+    (a0, a1, b0, b1) <- unwrapKeys kp1 kp2
+    (o0, o1) <- genKeyPair
     printTT $ TruthTable (a0, b0, o0) (a1, b0, o0) (a0, b1, o0) (a1, b1, o1)
     return (o0, o1)
 
 (.|.) :: IO (Key, Key) -> IO(Key, Key) -> IO (Key, Key)
 (.|.) kp1 kp2 = do
-    (a0, a1) <- kp1
-    (b0, b1) <- kp2
-    o0 <- getKey
-    o1 <- getKey
+    (a0, a1, b0, b1) <- unwrapKeys kp1 kp2
+    (o0, o1) <- genKeyPair
     printTT $ TruthTable (a0, b0, o0) (a1, b0, o1) (a0, b1, o1) (a1, b1, o1)
     return (o0, o1)
--- library/Utils.hs
+++ library/Utils.hs
@@ -1,8 +1,21 @@
 module Utils where
 import OpenSSL.Random
 import Data.Word
-import Data.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.LargeWord as LW (Word128, LargeKey(..))
+import Data.Binary.Get
 
-getKey :: IO ByteString
+getKey :: IO BS.ByteString
 getKey = randBytes 8
-    
+
+genKeyPair :: IO (BS.ByteString, BS.ByteString)
+genKeyPair = do
+    k1 <- randBytes 16
+    k2 <- randBytes 16
+    return (k1, k2)
+
+bsToWord128 :: BS.ByteString -> LW.Word128
+bsToWord128 s = let (s1, s2) = BS.splitAt 64 s in
+    let a1 = runGet getWord64le s2 in
+    let a2 = runGet getWord64le s1 in
+    a1
