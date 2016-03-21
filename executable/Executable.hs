cd30ae8f6c4535f2ec0965d051a6aad55f93c5df
Jacob Bisnett <jacob.bisnett@gmail.com>
2016-03-20 22:14:07 -0400

    better organization

--- executable/Executable.hs
+++ executable/Executable.hs
@@ -2,5 +2,9 @@ import Producer
 import Utils
 main :: IO ()
 main = do
-    _ <- (getKey, getKey) .&. (getKey, getKey)
+    k1 <- getKey
+    k2 <- getKey
+    k3 <- getKey
+    k4 <- getKey
+    _ <- return (k1, k2) .&. return (k3, k4)
     return ()
--- library/Producer.hs
+++ library/Producer.hs
@@ -5,7 +5,7 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
 import Utils
 
-type Key = (IO B.ByteString)
+type Key = B.ByteString
 data KeyPair = KeyPair{key1::Key, key2::Key}
 data TruthTable = TruthTable{
       row1 :: (Key, Key, Key)
@@ -23,19 +23,25 @@ printTT (TruthTable row1 row2 row3 row4 )= do
     return ()
     where
     printRow :: (Key, Key, Key) -> IO()
-    printRow (io1, io2, io3) = do
-        k1 <- io1
-        k2 <- io2
-        k3 <- io3
+    printRow (k1, k2, k3) = do
         print $ show (B8.unpack k1) ++ " " ++ show (B8.unpack k2) ++ " " ++ show (B8.unpack k3)
         return ()
 
 
-(.&.) (a0, a1) (b0, b1) = do
+(.&.) :: IO (Key, Key) -> IO(Key, Key) -> IO (Key, Key)
+(.&.) kp1 kp2 = do
+    (a0, a1) <- kp1
+    (b0, b1) <- kp2
     o0 <- getKey
     o1 <- getKey
-    let oi0 = return o0 :: (IO B.ByteString)
-    let oi1 = return o1 :: (IO B.ByteString)
-    printTT $ TruthTable (a0, b0, oi0) (a1, b0, oi0) (a0, b1, oi0) (a1, b1, oi1)
+    printTT $ TruthTable (a0, b0, o0) (a1, b0, o0) (a0, b1, o0) (a1, b1, o1)
     return (o0, o1)
 
+(.|.) :: IO (Key, Key) -> IO(Key, Key) -> IO (Key, Key)
+(.|.) kp1 kp2 = do
+    (a0, a1) <- kp1
+    (b0, b1) <- kp2
+    o0 <- getKey
+    o1 <- getKey
+    printTT $ TruthTable (a0, b0, o0) (a1, b0, o1) (a0, b1, o1) (a1, b1, o1)
+    return (o0, o1)
