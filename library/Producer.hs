8e1894573c200673e45b00c9da45e3a1c0849e44
Jacob Bisnett <jacob.bisnett@gmail.com>
2016-03-22 21:42:56 -0400

    some small QOL changes

--- library/Producer.hs
+++ library/Producer.hs
@@ -1,19 +1,16 @@
 module Producer where
 import qualified Data.ByteString as B
 import Data.ByteString.Internal (unpackBytes)
-import qualified Data.ByteString.Char8 as B8
 import qualified Codec.Binary.BubbleBabble as X
-import qualified Codec.Encryption.AES as C
-import Codec.Utils
-import Data.LargeWord
 import Utils
 
-type Key = Word128
+type Key = B.ByteString
 data KeyPair = KeyPair{key1::Key, key2::Key}
 data TruthTable = TruthTable (Key, Key, Key) (Key, Key, Key) (Key, Key, Key) (Key, Key, Key)
 
 printTT :: TruthTable -> IO()
 printTT (TruthTable row1 row2 row3 row4 )= do
+    putStrLn "Truth Table As Follows"
     printRow row1
     printRow row2
     printRow row3
@@ -23,7 +20,7 @@ printTT (TruthTable row1 row2 row3 row4 )= do
     printRow :: (Key, Key, Key) -> IO()
     printRow (k1, k2, k3) = do
         let s_list = map (X.encode . unpackBytes) [k1, k2, k3]
-        print $ unwords s_list
+        putStrLn $ unlines s_list
         return ()
 
 unwrapKeys :: IO(Key, Key) -> IO(Key, Key) -> IO(Key, Key, Key, Key)
@@ -32,24 +29,33 @@ unwrapKeys kp1 kp2 = do
     (b0, b1) <- kp2
     return (a0, a1, b0, b1)
 
-encryptKey :: (Word128, Word128, Word128) -> Word128
-encryptKey (k0, k1, m) = C.encrypt k0 . C.encrypt k1 m
-
 printSendTT :: TruthTable -> IO()
-printSendTT (TruthTable r1 r2 r3 r4) = 
-    let list = map encryptKey [r1, r2, r3, r4] in
-    print $ unlines list
+printSendTT (TruthTable r1 r2 r3 r4) =  do
+    let outkeys = map encOutKey [r1, r2, r3, r4]
+    -- network send
+    putStrLn "Encrypted Keys"
+    putStrLn $ unlines $ map (X.encode . unpackBytes) outkeys
 
+getKeys :: IO(Key, Key) -> IO(Key, Key) -> IO(Key, Key, Key, Key, Key, Key)
+getKeys kp1 kp2 = do
+    (a0, a1, b0, b1) <- unwrapKeys kp1 kp2 
+    (o0, o1) <- genKeyPair
+    return (a0, a1, b0, b1, o0, o1)
+
+sendInfo :: TruthTable -> IO()
+sendInfo tt = do
+    printTT tt
+    printSendTT tt
+
+-- define these as a type
 (.&.) :: IO (Key, Key) -> IO(Key, Key) -> IO (Key, Key)
 (.&.) kp1 kp2 = do
-    (a0, a1, b0, b1) <- unwrapKeys kp1 kp2
-    (o0, o1) <- genKeyPair
-    printTT $ TruthTable (a0, b0, o0) (a1, b0, o0) (a0, b1, o0) (a1, b1, o1)
+    (a0, a1, b0, b1, o0, o1) <- getKeys kp1 kp2
+    sendInfo $ TruthTable (a0, b0, o0) (a1, b0, o0) (a0, b1, o0) (a1, b1, o1)
     return (o0, o1)
 
 (.|.) :: IO (Key, Key) -> IO(Key, Key) -> IO (Key, Key)
 (.|.) kp1 kp2 = do
-    (a0, a1, b0, b1) <- unwrapKeys kp1 kp2
-    (o0, o1) <- genKeyPair
-    printTT $ TruthTable (a0, b0, o0) (a1, b0, o1) (a0, b1, o1) (a1, b1, o1)
+    (a0, a1, b0, b1, o0, o1) <- getKeys kp1 kp2
+    sendInfo $ TruthTable (a0, b0, o0) (a1, b0, o1) (a0, b1, o1) (a1, b1, o1)
     return (o0, o1)
--- library/Utils.hs
+++ library/Utils.hs
@@ -1,21 +1,42 @@
 module Utils where
 import OpenSSL.Random
 import Data.Word
+import Data.Monoid
+import Data.Maybe
 import qualified Data.ByteString as BS
-import qualified Data.LargeWord as LW (Word128, LargeKey(..))
-import Data.Binary.Get
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Builder as D
+import Crypto.Cipher.AES
+import Crypto.Cipher.Types
+import Crypto.Error
 
-getKey :: IO BS.ByteString
-getKey = randBytes 8
+zeroBuilder :: D.Builder
+zeroBuilder = mappend (D.word32LE 0) (D.word16LE 0)
 
 genKeyPair :: IO (BS.ByteString, BS.ByteString)
 genKeyPair = do
-    k1 <- randBytes 16
-    k2 <- randBytes 16
-    return (k1, k2)
+    k1 <- randBytes 10
+    k2 <- randBytes 10
+    let k1Builder = mappend (D.byteString k1) zeroBuilder
+    let k2Builder = mappend (D.byteString k2) zeroBuilder
 
-bsToWord128 :: BS.ByteString -> LW.Word128
-bsToWord128 s = let (s1, s2) = BS.splitAt 64 s in
-    let a1 = runGet getWord64le s2 in
-    let a2 = runGet getWord64le s1 in
-    a1
+    return (L.toStrict . D.toLazyByteString $ k1Builder, L.toStrict . D.toLazyByteString $ k2Builder)
+    
+getAESKeys :: BS.ByteString -> BS.ByteString -> (AES128, AES128)
+getAESKeys a b = (throwCryptoError $ cipherInit a :: AES128, throwCryptoError $ cipherInit b :: AES128)
+
+encOutKey :: (BS.ByteString, BS.ByteString, BS.ByteString) -> BS.ByteString
+encOutKey (a, b, o) =
+    let (ac, bc) = getAESKeys a b in 
+    ecbEncrypt ac $ ecbEncrypt bc o
+
+decOutKey :: (BS.ByteString, BS.ByteString, BS.ByteString) -> Maybe BS.ByteString
+decOutKey (a, b, o) = 
+    let (ac, bc) = getAESKeys a b in 
+    let check = ecbDecrypt bc $ ecbDecrypt ac o in
+    let zeros = L.toStrict . D.toLazyByteString $ zeroBuilder in
+    let (val, z) = BS.splitAt 80 check in
+    if z == zeros
+        then Just check
+        else Nothing
+    
