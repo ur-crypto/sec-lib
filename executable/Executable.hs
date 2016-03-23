666741a0998a3bcbccf7a670e7232ccefe6f85c9
Jacob Bisnett <jacob.bisnett@gmail.com>
2016-03-22 21:43:16 -0400

    the rest of prev commit

--- executable/Executable.hs
+++ executable/Executable.hs
@@ -1,10 +1,11 @@
 import Producer
 import Utils
+
 main :: IO ()
 main = do
-    k1 <- getKey
-    k2 <- getKey
-    k3 <- getKey
-    k4 <- getKey
-    _ <- return (k1, k2) .&. return (k3, k4)
+    (k1, k2) <- genKeyPair
+    (k3, k4) <- genKeyPair
+    (k5, k6) <- genKeyPair
+    (o1, o2) <- return (k1, k2) .&. return (k3, k4)
+    _ <- return (k5, k6) .|. return (o1, o2)
     return ()
