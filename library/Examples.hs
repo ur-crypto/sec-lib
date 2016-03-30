338eaa72c7bc0c3687961dce4e0c583efbc10819
Jacob Bisnett <jacob.bisnett@gmail.com>
2016-03-30 00:25:46 -0400

    Fixed the GT test, renamed the GT test to PltC (since it is actually if the producer is less than the consumer, theres some ambiguity of ordering throught this so if the order matters from now on I will explicitly state it. Also added a test that checks if our program correctly compares all 8 bit integers (it does, ran in just over 3 minutes)

--- library/Examples.hs
+++ library/Examples.hs
@@ -19,14 +19,14 @@ test64 = 1234567890
 testb64 :: Int64
 testb64 = 111111
 
-numGT :: TestBool a
-numGT [n1] [n2] = 
+numPltC :: TestBool a
+numPltC [n1] [n2] = 
        let nbool = Gate NAND n2 n2 in
        n1 .&. nbool
-numGT (n1:n1s) (n2:n2s) = 
+numPltC (n1:n1s) (n2:n2s) =
        let nbool = Gate NAND n2 n2 
            mbool = Gate NAND n1 n1 in
-       ifThenElse ( n1 .&. nbool) n1 (ifThenElse (mbool .&. n2) n2 (numGT n1s n2s))
+       ifThenElse ( n1 .&. nbool) n1 (ifThenElse (mbool .&. n2) n1 (numPltC n1s n2s))
 
 numCmp :: TestBool a
 numCmp n1 n2 = foldl1 (.&.) (zipWith bij n1 n2)
