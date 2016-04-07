3a4d295755989da05a56bce19c8aeb09803fb9d3
Muthuramakrishnan Venkitasubramaniam <muthuv@cs.rochester.edu>
2016-04-06 22:05:50 -0400

    multibit

--- executable/Executable.hs
+++ executable/Executable.hs
@@ -19,12 +19,12 @@ parseArgs _ = Nothing
 
 doArgs :: Maybe Mode -> IO()
 doArgs (Just Producer) = do 
-    (res0, res1) <- P.doWithoutSocket (testb64, test64) numGT
+    (res0, res1) <- P.doWithoutSocket (testb64, test64) caddInt 
     printKey (Just False) res0
     printKey (Just True) res1
     return ()
 doArgs (Just Consumer) = do 
-    res <- C.doWithoutSocket (testb64, test64) numGT
+    res <- C.doWithoutSocket (testb64, test64) caddInt
     printKey Nothing res
     return ()
 doArgs Nothing = usage
--- library/Examples.hs
+++ library/Examples.hs
@@ -13,11 +13,32 @@ test16 = 124
 test32 :: Int32
 test32 = 1234567890
 
-test64 :: Int64
-test64 = 1234567890
-
 testb64 :: Int64
-testb64 = 111111
+testb64 = 64 
+
+test64 :: Int64
+test64 = 64
+
+caddInt [n1] [n2] = (n1 .&. n2)
+
+caddInt (n1:n1s) (n2:n2s) = 
+       let xsum = Gate XOR n1 n2
+           asum = (n1 .&. n2)
+           x = caddInt n1s n2s in
+       (asum .|. (xsum .&. x)) 
+
+
+
+addInt [n1] [n2] =
+       let xsum = Gate XOR n1 n2
+           asum = (n1 .&. n2) in
+       (asum:xsum:[])
+addInt (n1:n1s) (n2:n2s) = 
+       let xsum = Gate XOR n1 n2
+           asum = (n1 .&. n2)
+           x = head(addInt n1s n2s)
+           xs = tail (addInt n1s n2s) in
+       (((asum .|. (xsum .&. x)):(Gate XOR xsum x):[])++xs) 
 
 numGT :: TestBool a
 numGT [n1] [n2] = 
@@ -26,7 +47,7 @@ numGT [n1] [n2] =
 numGT (n1:n1s) (n2:n2s) = 
        let nbool = Gate NAND n2 n2 
            mbool = Gate NAND n1 n1 in
-       ifThenElse ( n1 .&. nbool) n1 (ifThenElse (mbool .&. n2) n2 (numGT n1s n2s))
+       ifThenElse ( n1 .&. nbool) n1 (ifThenElse (mbool .&. n2) n1 (numGT n1s n2s))
 
 numCmp :: TestBool a
 numCmp n1 n2 = foldl1 (.&.) (zipWith bij n1 n2)
