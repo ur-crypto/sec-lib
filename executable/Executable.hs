3c5df515e55fa5faeea9a1734e8e3f62e5571453
Jacob Bisnett <jacob.bisnett@gmail.com>
2016-04-08 19:03:39 -0400

    added a better prof test

--- executable/Executable.hs
+++ executable/Executable.hs
@@ -2,11 +2,13 @@ import Prelude
 import Utils
 import qualified Consumer as C
 import qualified Producer as P
+import TestUtils
+import Control.Concurrent.Async
 import Examples
 import System.Environment
 
     
-data Mode = Producer | Consumer
+data Mode = Producer | Consumer | Both
 
 usage :: IO()
 usage = putStrLn "Enter producer or consumer"
@@ -14,6 +16,7 @@ usage = putStrLn "Enter producer or consumer"
 parseArgs :: [String] -> Maybe Mode
 parseArgs ("producer":_) = Just Producer
 parseArgs ("consumer":_) = Just Consumer
+parseArgs ("both":_) = Just Both
 parseArgs [] = Nothing
 parseArgs _ = Nothing
 
@@ -27,6 +30,21 @@ doArgs (Just Consumer) = do
     res <- C.doWithoutSocket (testb64, test64) numPltC
     printKey Nothing res
     return ()
+doArgs (Just Both) = do
+    hcsoc <- async C.getSocket
+    hpsoc <- async P.getSocket
+    csoc <- wait hcsoc
+    psoc <- wait hpsoc
+    conOutHandle <- asyncBound $ C.doWithSocket csoc (test16, test16) numCmp
+    proOutHandle <- asyncBound $ P.doWithSocket psoc (test16, test16) numCmp
+    conOut <- wait conOutHandle
+    (proOut0, proOut1) <- wait proOutHandle
+    printKey Nothing conOut
+    printKey (Just False) proOut0
+    printKey (Just True) proOut1
+    return ()
+
+
 doArgs Nothing = usage
 
 main :: IO()
