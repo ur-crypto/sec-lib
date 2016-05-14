0d7d3d2f17544572af6b38fb15da07ba30f8ab53
Jacob Bisnett <jacob.bisnett@gmail.com>
2016-05-13 23:31:31 -0800

    possible point and permute

--- library/Gate.hs
+++ library/Gate.hs
@@ -22,13 +22,13 @@ class LocalValue v where
         !x <- process soc fkeys k1
         !y <- process soc fkeys k2
         case (x, y) of
-            ((Input a), (Input b)) ->
+            (Input a, Input b) ->
                 gateHandler soc fkeys ty a b
-            ((Input _), (Constant b)) ->
+            (Input _, Constant b) ->
                 processConstant ty x b
-            ((Constant a), (Input _)) ->
+            (Constant a, Input _) ->
                 processConstant ty y a
-            ((Constant a), (Constant b)) ->
+            (Constant a, Constant b) ->
                 return $ case ty of
                     AND -> Constant (a && b)
                     OR -> Constant (a || b)
@@ -56,13 +56,13 @@ instance LocalValue (Key, Key) where
     notHandler (Input (k1, k2)) = return $ Input (k2, k1)
     notHandler (Constant l) = return $ Constant $ not l
     notHandler _ = error "Should not recieve Gate"
-    gateHandler _ _ XOR (a0, a1) (b0, b1) =
+    gateHandler _ _ XOR (a0, _) (b0, b1) =
         let o1 = BS.pack $ BS.zipWith xor a0 b0
             o2 = BS.pack $ BS.zipWith xor a0 b1 in
         return (Input (o1, o2))
     gateHandler (Just soc) [AES fkey,RAND rkey] ty a b = do
         ok <- genKeyPair rkey
-        let o = (Input ok)
+        let o = Input ok
         let tt = getTT ty o (Input a) (Input b)
         sendInfo tt
         return o
@@ -79,10 +79,9 @@ instance LocalValue (Key, Key) where
             helper _ _ _ _ _ _ = error "Should only pass values"
 
             sendInfo :: PTT -> IO()
-            sendInfo (TruthTable r1 r2 r3 r4) = do
-                outkeys <- shuffle $ map (encOutKey fkey) [r1, r2, r3, r4]
-                SBS.sendMany soc outkeys
+            sendInfo tt = SBS.sendMany soc $ encTruthTable fkey tt
     gateHandler Nothing _ _ _ _ = error "Needs a socket"
+    gateHandler _ _ _ _ _ = error "Needs Correct Keys"
 
 instance LocalValue Key where
     notHandler (Input a) = return $ Input a
@@ -92,7 +91,7 @@ instance LocalValue Key where
         return $ Input $ BS.pack $ BS.zipWith xor a b
     gateHandler (Just soc) [AES fkey] _ x y = do
         tt <- getTT
-        return $ processTT tt x y
+        return $ Input $ decTruthTable fkey x y tt
         where
             getTT = do
                 byteTT <- SBS.recv soc (4 * cipherSize)
@@ -100,16 +99,8 @@ instance LocalValue Key where
                 let (x2, r2) = BS.splitAt cipherSize r1
                 let (x3, x4) = BS.splitAt cipherSize r2
                 return $ TruthTable x1 x2 x3 x4
-            processTT (TruthTable a b c d) k1 k2 =
-                let (Just k) = head $ filter corrKey $ map (decOutKey fkey) keyList in
-                Input k
-                where
-                corrKey :: Maybe Key -> Bool
-                corrKey (Just _) = True
-                corrKey Nothing = False
-                keyList =  [(k1, k2, a), (k1, k2, b), (k1, k2, c), (k1, k2, d)]
-            processTT  _ _ _ = error "Passing gate to processTT"
     gateHandler Nothing _ _ _ _ = error "Needs a socket"
+    gateHandler _ _ _ _ _ = error "Needs Correct Keys"
 
 incCounter :: Int -> GateCounter -> GateCounter
 incCounter i =
@@ -126,11 +117,13 @@ instance LocalValue GateCounter where
             type2Int XOR = 2
             type2Int BIJ = 3
             type2Int NAND = 4
+    gateHandler _ _ _ _ _ = error "Does not use socket"
 
 instance LocalValue Int where
     notHandler (Input i) = return $ Input $ i+1
     notHandler other = return other
     gateHandler Nothing _ _ ai bi = return $ Input $ ai + bi + 1
+    gateHandler _ _ _ _ _ = error "Does not use socket"
 
 countGates :: Int -> SecureFunction Int -> IO()
 countGates inputs func = do
