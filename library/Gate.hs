c4bccce23e708b469c7c45fbfca09985e9fec2f2
Jacob Bisnett <jacob.bisnett@gmail.com>
2016-05-13 19:08:26 -0800

    revert to working gate

--- library/Gate.hs
+++ library/Gate.hs
@@ -1,14 +1,15 @@
-{-# LANGUAGE BangPatterns         #-}
-{-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE BangPatterns #-}
 module Gate where
-import           Data.Bits
-import qualified Data.ByteString           as BS
-import           Data.List
-import           Network.Socket
+import Utils
+import Types
+import Network.Socket
 import qualified Network.Socket.ByteString as SBS
-import           Types
-import           Utils
+import qualified Data.ByteString as BS
+import Data.Bits
+import Data.List
+import Control.Monad
 
 data KeyContext = AES FixedKey
                 | RAND Key
@@ -18,28 +19,36 @@ type GateCounter = [Int]
 class LocalValue v where
     gateHandler :: Maybe Socket -> [KeyContext] -> GateType -> v -> v -> IO (Node v)
     notHandler :: Node v -> IO (Node v)
-    processConstant :: GateType -> Node v -> Node v -> Node v
-    processConstant ty (Constant a) (Constant b) =
-        Constant  $ case ty of
-            AND -> a && b
-            OR -> a || b
-            XOR -> if a then not b else b
-            BIJ -> not $ if a then not b else b
-            NAND -> not $ (&&) a b
-    processConstant ty a (Constant b) =
-        case ty of
-            AND -> if b then a else Constant False
-            OR -> if b then Constant True else a
-            XOR -> if b then Not a else a
-            BIJ -> if b then a else Not a
-            NAND -> if b then Not a else Constant True
-    processConstant ty (Constant b) a = processConstant ty a (Constant b)
-    processConstant ty a b = Gate ty a b
     process :: Maybe Socket -> [KeyContext] -> Node v -> IO (Node v)
-    process soc fkeys (Gate ty !k1 !k2) = do
-        (Input x) <- process soc fkeys k1
-        (Input y) <- process soc fkeys k2
-        gateHandler soc fkeys ty x y
+    process soc fkeys (Gate ty k1 k2) = do
+        x <- process soc fkeys k1
+        y <- process soc fkeys k2
+        case (x, y) of
+            ((Input a), (Input b)) -> 
+                gateHandler soc fkeys ty a b
+            ((Input _), (Constant b)) ->
+                processConstant ty x b
+            ((Constant a), (Input _)) ->
+                processConstant ty y a
+            ((Constant a), (Constant b)) ->
+                return $ case ty of
+                    AND -> Constant (a && b)
+                    OR -> Constant (a || b)
+                    XOR -> Constant (xor a b)
+                    NAND -> Constant (not (a && b))
+                    BIJ -> Constant (not (xor a b))
+            (_, _) -> error "process gate returning wrong value"
+        where
+        processConstant AND _ False = return $ Constant False
+        processConstant AND key True = return key
+        processConstant OR key False = return key
+        processConstant OR _ True = return $ Constant True
+        processConstant XOR key False = return key
+        processConstant XOR key True = process soc fkeys (Not key)
+        processConstant NAND _ False = return $ Constant True
+        processConstant NAND key True = return key
+        processConstant BIJ key False = process soc fkeys (Not key)
+        processConstant BIJ key True = return key
     process soc fkeys (Not node) = do
         procNode <- process soc fkeys node
         notHandler procNode
@@ -107,7 +116,6 @@ instance LocalValue Key where
 incCounter :: Int -> GateCounter -> GateCounter
 incCounter i =
     zipWith (\ind val -> if ind == i then val + 1 else val) [0..]
-
 instance LocalValue GateCounter where
     notHandler (Input gc) = return $ Input $ incCounter 5 gc
     notHandler other = return other
@@ -126,12 +134,9 @@ instance LocalValue Int where
     notHandler other = return other
     gateHandler Nothing _ _ ai bi = return $ Input $ ai + bi + 1
 
-countGates :: Int -> SecureFunction GateCounter -> IO()
+countGates :: Int -> SecureFunction Int -> IO()
 countGates inputs func = do
-    let inp = map (const $ Input [0,0,0,0,0,0]) [0..inputs-1]
+    let inp = map (const $ Input 0) [0..inputs-1]
     let tree = func inp inp
-    res <- mapM (process Nothing []) tree
-    print $ foldl' combine [0,0,0,0,0,0] res
-        where
-            combine acc (Input a) = zipWith (+) acc a
-            combine acc _ = acc
+    res <- process Nothing [] $ head tree
+    print res
