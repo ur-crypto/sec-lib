61ca0d05f5ff2b7691b4f2e66f9875d0fa4e5833
Muthuramakrishnan Venkitasubramaniam <muthuv@cs.rochester.edu>
2016-05-16 21:56:09 -0400

    trying (3,3) now

--- library/Examples.hs
+++ library/Examples.hs
@@ -100,8 +100,8 @@ ourBool [] = []
 
 logCeil :: Int -> Int
 logCeil i = case (i>1) of
-            (True) -> 2--1 + floor ( logBase 2 (fromIntegral i))
-            (False) -> 2--1
+            (True) -> ceil ( logBase 2 (fromIntegral i))
+            (False) -> 2
 
 editDist :: SecureFunction a
 editDist xs ys =  --let xss = (take 2 xs) 
@@ -111,7 +111,7 @@ editDist xs ys =  --let xss = (take 2 xs)
 
 editDistEff :: Int -> [Node a] -> [[Node a]] -> SecureFunction a
 editDistEff i topValue es (x:xs) ys =
-          case (i < 3) of  
+          case (i < 4) of  
              (True) ->     let (_,(carry,subTotal)) = addIntFP (logCeil i) topValue [Constant True] in
                              let prev = [carry]++subTotal in
                                let updatedColumn = columnCalc i 1 [prev]  es x ys in
@@ -121,7 +121,7 @@ editDistEff _ _ es _ _ = trace ("We are done exploring the graph of Nodes.") (la
 
 columnCalc :: Int -> Int -> [[Node a]]  -> [[Node a]] -> Node a -> [Node a] -> [[Node a]]
 columnCalc i j curColumn  (e1:es@(e2:es')) x (y:ys) =
-      case (j < 3) of
+      case (j < 4) of
        (True) -> let addValue = b_xor x y 
                      prev = last curColumn in
                   let (_,(carry,subTotal)) = addIntFP (logCeil (max i j)) e1 [addValue] in
